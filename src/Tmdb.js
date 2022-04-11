eaeef98d43f6649dbe8239353871fc0cdd559844
Sandra Rute <rute.leone@hotmail.com>
2022-04-11 20:12:25 -0300

    Atualização do layout, impletação header

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -50,6 +50,25 @@ export default {
                 title: 'Documentários',
                 items: await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
+
         ];
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+        
+        if (movieId) {
+            switch(type) {
+                case 'movie':
+                    info = await basicFecth(`/movie/${movieId}? language=pt-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    info = await basicFecth(`/tv/${movieId}? language=pt-BR&api_key=${API_KEY}`);
+                break;
+                default: 
+                    info = null;
+                break;
+            }
+        }
+        return info;
     }
 }
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieRow.css';
+import { MessageTwoTone, NavigateBefore, NavigateNext } from "@material-ui/icons";
 
 export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(-400);  
+
+    const handleLeftArrow  = () => {
+        let x = scrollX + Math.round(window.innerWidth /2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+    const handleRightArrow = () => {
+
+    }
+
     return (
         <div className='movieRow'>
             <h2>{title}</h2>
+            <div className='movieRow--left' onClick={handleLeftArrow}>
+                <NavigateBefore style={{fontSize:50}} />
+            </div>
+            <div className='movieRow--right' onClick={handleRightArrow}>
+                < NavigateNext style={{fontSize:50}}/>
+            </div>
             <div className='movieRow--listarea'>
-                <div className='movieRow--list'>
+                <div className='movieRow--list' style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
                     {items.results.length >0 && items.results.map( (item, key) => (
                         <div key={key} className='movieRow--item'>
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
